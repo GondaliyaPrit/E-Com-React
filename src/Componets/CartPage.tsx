af2bf0b2681ecde1943de53b522b51ebfe05cb8c
GondaliyaPrit <prit.github@gmail.com>
2022-10-06 12:37:49 +0530

    create drwer design

--- src/Componets/CartPage.tsx
+++ src/Componets/CartPage.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
-import bag from "../img/bag.png";
-import Button from "@mui/material/Button";
-type Anchor = "top" | "left" | "bottom" | "right";
+import bag from "../img/bag.svg"
+import close from "../img/close.svg"
+import cartemp from "../img/empcart.png"
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+type Anchor = "right";
 
 function CartPages() {
   const [state, setState] = React.useState({
@@ -24,7 +28,7 @@ function CartPages() {
   
   const list = (anchor: Anchor) => (
     <Box
-      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 450 }}
+      sx={{ width:450 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
@@ -32,7 +36,17 @@ function CartPages() {
       <div className="cart-wrap">
         <div className="cart-head">
         <h2 className="cart-tittle">Shopping cart</h2>
+        <img src={close} alt="cart" className="cart-btn" onClick={toggleDrawer("right", false)}/>
         </div>
+        <div className="card-mid-wrap">
+        <img src={cartemp} alt="cart" className="empcart" onClick={toggleDrawer("right", false)}/>
+        <h2 className="cart-emp-tittle" >Your cart is empty !</h2>
+        </div>
+        <div className="checkout-btn">
+          <h3 className="checkout-btn-tittle">Proceed To Checkout</h3>
+          <h3 className="checkout-total">| {"$0.0"}</h3>
+       
+        </div> 
       </div>
     </Box>
   );
@@ -41,7 +55,6 @@ function CartPages() {
     <div>
       <React.Fragment key={"right"}>
       <img src={bag} alt="cart" className="cart-btn" onClick={toggleDrawer("right", true)}/>
-        {/* <Button onClick={toggleDrawer("right", true)}>{""}</Button> */}
         <Drawer
           anchor={"right"}
           open={state["right"]}
